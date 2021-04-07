d1c9a143d69a8835cf0f08d6ef8ad2794db576ec
andy0025681 <andy0025681@gmail.com>
2021-04-08 05:19:41 +0800

    login view

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from flask_login import UserMixin
+from . import db, login_manager
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
