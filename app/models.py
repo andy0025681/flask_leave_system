3dacbfd225350d5feda444d72b54e5fa8d1b992f
andy0025681 <andy0025681@gmail.com>
2021-04-10 23:48:35 +0800

    offical leave

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import calendar
 import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -22,10 +23,39 @@ class Status:
     AGREE = 4
 
 class Time():
+    # 日期間格。
+    @staticmethod
+    def dateInterval(start, end):
+        if(start > end):
+            return False
+        year = end.year - start.year
+        month = day = 0
+        if(start.month > end.month):
+            year -= 1
+            month = (12 - start.month) + end.month
+        else:
+            month = end.month - start.month
+        if(start.day > end.day):
+            if(month > 0):
+                month -= 1
+            else:
+                year -= 1
+                month = 11
+            if(end.month <= 1):
+                day = (calendar.monthrange(end.year-1, 12)[1] - start.day) + end.day
+            else:
+                day = (calendar.monthrange(end.year, end.month-1)[1] - start.day) + end.day
+        else:
+            day = end.day - start.day
+        # print("{}-{}-{}".format(year, month, day))
+        return [year, month, day]
+
+    # 是否為工作日。
     @staticmethod
     def workday():
         return True
 
+    # 當日工時。
     @staticmethod
     def workingHours_day(start, end):
         fmt = '%H:%M:%S'
@@ -59,6 +89,7 @@ class Time():
                 else:
                     return (evening-afterNoon).seconds
     
+    # 期間工時。
     @staticmethod
     def workingHours_days(start, end):
         seconds = 0
@@ -82,6 +113,82 @@ class Time():
                 seconds += 8*60*60
             date += timedelta(days=1)
         return seconds
+
+class OfficalLeave:
+    # 計算年資，並轉為特休天數。
+    @staticmethod
+    def seniorityToLeave(start, end):
+        seniority = Time.dateInterval(start, end)
+        if(not seniority):
+            return -1
+        buf = [ False, 7, 10, 14, 14, # 0~4年
+                15, 15, 15, 15, 15, # 5~9年
+                16, 17, 18, 19, 20, # 10~14年
+                21, 22, 23, 24, 25, # 15~19年
+                26, 27, 28, 29, 30 ] # 20~24年以上
+        if(seniority[0] >= 1):
+            return buf[seniority[0]]
+        else:
+            return 3 if(seniority[1] > 0 or seniority[2] > 0) else 0
+
+    # 年資比例: 用來計算特休比例。年資比例依照到職日劃分為上半年與下半年。
+    @staticmethod
+    def yearProportion(firstDay, seniorityRange):
+        res = ((firstDay.month - 1) + (firstDay.day - 1) / calendar.monthrange(firstDay.year, firstDay.month)[1]) / seniorityRange
+        return 1 if res > 1 else res
+    
+    # 以到職日計算某年特休開放日期。
+    @staticmethod
+    def startDate(firstDay, thatYear):
+        if(firstDay >= datetime.strptime('{}-1-1'.format(thatYear+1), '%Y-%m-%d')):
+            return False
+        thatYear = datetime.strptime('{}-1-1'.format(thatYear), '%Y-%m-%d')
+        month = firstDay.month+6
+        year = firstDay.year
+        if(month > 12):
+            month -= 12
+            year += 1
+        firstOffStartDate = datetime.strptime('{}-{}-{}'.format(year, month, firstDay.day), '%Y-%m-%d')
+        return thatYear if(firstOffStartDate < thatYear) else firstOffStartDate
+
+    # 以到職日計算某年特休天數 (新年限定)
+    @staticmethod
+    def newYear(firstDay, thatYear):
+        result = 0
+        firstYear = datetime.strptime('{}-01-01'.format(thatYear), '%Y-%m-%d')
+        if(firstDay > firstYear):
+            return False
+        nextYear = thatYear + 1
+        # 去年開始的年資的下半部分
+        offLeave = OfficalLeave.seniorityToLeave(firstDay, firstYear)
+        seniorityRange = 6 if offLeave <= 3 else 12
+        lastOffLeave = OfficalLeave.yearProportion(firstDay, seniorityRange) * offLeave
+        result += lastOffLeave
+        # 今年開始的年資的上半部分
+        offLeave = OfficalLeave.seniorityToLeave(firstDay, firstYear) \
+            if(firstDay.month == 1 and firstDay.day == 1) \
+                else OfficalLeave.seniorityToLeave(firstDay, datetime.strptime('{}-01-01'.format(nextYear), '%Y-%m-%d'))
+        seniorityRange = 6 if offLeave <= 3 else 12
+        thisOffLeave = offLeave - (OfficalLeave.yearProportion(firstDay, seniorityRange) * offLeave)
+        result += thisOffLeave
+        result = round(round(round(result, 3), 2), 1)
+        return [datetime.strptime('{}-01-01'.format(thatYear), '%Y-%m-%d'), datetime.strptime('{}-12-31'.format(thatYear), '%Y-%m-%d'), result]
+
+    # 以到職日計算當年計算特休天數 (新員工限定)
+    @staticmethod
+    def newStaff(firstDay):
+        start = OfficalLeave.startDate(firstDay, firstDay.year)
+        end = datetime.strptime('{}-12-31'.format(firstDay.year), '%Y-%m-%d')
+        if(start > end):
+            return [False, False, 0]
+        thisYear = datetime.strptime('{}-12-31'.format(firstDay.year), '%Y-%m-%d')
+        # 今年開始的年資的上半部分
+        offLeave = OfficalLeave.seniorityToLeave(firstDay, thisYear)
+        seniorityRange = 6 if offLeave <= 3 else 12
+        result = offLeave - (OfficalLeave.yearProportion(firstDay, seniorityRange) * offLeave)
+        print("{} - ({} * {})".format(offLeave, OfficalLeave.yearProportion(firstDay, seniorityRange), offLeave))
+        result = round(round(round(result, 3), 2), 1)
+        return [start, end, result]
 
 class LeaveType(db.Model):
     __tablename__ = 'leave_types'
@@ -156,6 +263,10 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     department_id = db.Column(db.Integer, db.ForeignKey('deparements.id'))
     password_hash = db.Column(db.String(128))
+    firstDay = db.Column(db.DateTime)
+    off_leave_start = db.Column(db.DateTime)
+    off_leave_end = db.Column(db.DateTime)
+    officalLeave = db.Column(db.Float)
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
@@ -180,6 +291,8 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = self.gravatar_hash()
+        self.firstDay = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d')
+        self.off_leave_start, self.off_leave_end, self.officalLeave = OfficalLeave.newStaff(self.firstDay)
     
     @property
     def password(self):
